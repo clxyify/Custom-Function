bcbfd639f2a208dc303ea74e50331c7f3f369250
clxyify <98536033+clxyify@users.noreply.github.com>
2022-04-01 10:03:25 -0400

    Create CF.lua

---
+++ CF.lua
@@ -0,0 +1,8 @@
+--run this function before using the function
+local function printsomething(text)
+print(test)
+wait(0.05)
+end)
+
+--clear the code on the top and run this code below after you remove the code from the top
+printsomething("hello i just printed without the global 'print' wow isn't that cool!")
